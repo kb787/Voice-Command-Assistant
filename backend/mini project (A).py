fb1d72e7931ee8ba185f0a293057e06f54bfa207
kb787 <120624674+kb787@users.noreply.github.com>
2023-04-27 18:05:49 +0530

    Update mini project (A).py

--- backend/mini project (A).py
+++ backend/mini project (A).py
@@ -1,5 +1,3 @@
-
-
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -7,6 +5,7 @@ import wikipedia
 import webbrowser
 import os
 import smtplib as smtp
+import pyautogui 
 
 jarv = pyttsx3.init('sapi5')
 voices = jarv.getProperty('voices')
@@ -23,10 +22,10 @@ def greet():
 
       elif time>= 12 and time < 16:
        speak(" Good Afternoon sir what can I do for you ")   
-                 
+
       elif time>=16 and time < 20:
        speak("  Good Evening sir what can I do for you  ")  
- 
+
       elif time>=20 and time < 24:
        speak(" Good Night sir what can I do for you  ")          
 
@@ -41,7 +40,7 @@ def listen():
         command = rec.recognize_google(audio, language='english')
         print(command)
     return command
-                
+
 if__name__ = "__main__"  
 greet()
 
@@ -106,8 +105,9 @@ while True :
         webbrowser.open("https://www.leetcode.com")
     elif "take a break" in task : 
         speak("Thanks sir for your free time , have a good day ")
+        exit()
     elif "give your intro" in task :
-        speak(" I am assistant created by KB sir and company an AI which uses speech recognition designed in Python. Various modules had been used pyttsx3 for giving sound , speech recognition for converting speech to text , os module for executing command opening a file from directory , smtplib for sending mails, tkinter for gui development. I can perform various tasks like sending an e-mail , playing song , opening variuos websites.I hope you enjoy your time with me  ")  
+        speak(" I am assistant created by KB sir and company an AI which uses speech recognition designed in Python. Various modules had been used pyttsx3 for giving sound , speech recognition for converting speech to text , os module for executing command opening a file from directory , smtplib for sending mails . I can perform various tasks like sending an e-mail ,opening variuos websites, opening notepad, other application.I can also perform voice to text typing hope you enjoy your time with me  ")  
     elif "group info" in task :
         speak(" Person 1 : Karan Bhanushali , SE-1 , Cmpn roll no 9. Person 2 : Nikhil Joshi , SE-1 , Cmpn roll no 47. Person 3 : Sumit Anantwal , SE-1 , Cmpn roll no 5 . Person 4 : Chetan Edge , SE-1 , Cmpn Roll no 29  ")              
     elif "send email" in task :
@@ -115,26 +115,26 @@ while True :
         person2content = listen().lower()
         server = smtp.SMTP('smtp.gmail.com',587)
         server.starttls()
-        server.login('karanbhanu799@gmail.com','xbxpkopvalhqispf')
+        server.login('youremail@gmail.com','password')
         server.sendmail('karanbhanu799@gmail.com','karanbhanu787@gmail.com',person2content)
-        speak(" Successfully parsed your message from karanbhanu799@gmail.com " +  " The message is" + person2content) 
+        speak(" Successfully parsed your message from freakywebdev@gmail.com " +  " The message is" + person2content) 
         server.close()   
-    elif "show me important documents"  in task :
+    elif "open important documents"  in task :
           impDoc = "C:\\Users\\Admin\\Desktop\\KB\\Imp Doc"
           os.startfile(impDoc)     
-    elif "show me android studio folder" in task :
+    elif "open android studio folder" in task :
           androidStudio = "C:\\Program Files\\Android"
           os.startfile(androidStudio)
-    elif "launch notepad" in task :
+    elif "open notepad" in task :
           notePad = "C:\\Windows\\Notepad.exe"
           os.startfile(notePad)     
-    elif  "launch eclipse" in task :
+    elif  "open eclipse" in task :
           eclipse = "C:\\Users\\Admin\\eclipse\\java-2022-095\\eclipse"
           os.startfile(eclipse)
-    elif "launch code blocks" in task :
+    elif "open code blocks" in task :
           codeblocks = "C:\\Program Files\\CodeBlocks\\codeblocks.exe"
           os.startfile(codeblocks) 
-    elif "launch mysql workbench" in task :
+    elif "open mysql workbench" in task :
           mysql = "C:\\Program Files\\MySQL\MySQL Workbench 8.0 CE\\MySQLWorkbench.exe"
           os.startfile(mysql)      
     elif "open ppt" in task :
@@ -146,8 +146,17 @@ while True :
     elif "open log book" in task :
           logbook = "C:\\Users\\Admin\\Desktop\\KB\\Imp Doc\\logbookfinal.docx"
           os.startfile(logbook)
+    elif "type notes" in task : 
+          filePath = "C:\\Users\\Admin\\Desktop\\KB\\Imp Doc\\assistant.txt"
+          os.startfile(filePath)
+          speak("Enter the text to write in notes") 
+          takeNote = listen()
+          pyautogui.typewrite(takeNote)                   
     else :
-        speak(" Repeat the command once again please ")           
+        speak(" Repeat the command once again please ")     
+        
+
+         
         
 
          
